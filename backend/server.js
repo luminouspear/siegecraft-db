255ce59a52c30153d50c2f56ccd62242a34d35b8
luminouspear <luminouspears@gmail.com>
2023-02-06 14:31:56 -0500

    updated server routing

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ app.use("/api/cards", cardRoutes);
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "public/index.html"));
+	res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 
