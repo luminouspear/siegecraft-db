206b22cdfa99562a5e4d835c0240a3c7336fd2b2
luminouspear <luminouspears@gmail.com>
2023-02-06 18:00:07 -0500

    updated server routing (paths)

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ app.use(express.static(path.join(__dirname, "client/build")));
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static(path.join(__dirname, "client/build")));
 	app.get("*", (req, res) => {
-		res.sendFile("../frontend/build/index.html"));
+		res.sendFile("../frontend/build/index.html");
 	});
 }
 
